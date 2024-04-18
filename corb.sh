c802a71c38fa464b591f73be6d061d678487b68a
Hari Krishna <hari.100.hks@outlook.com>
2024-04-18 15:32:32 +0530

    Update corb.sh

--- corb.sh
+++ corb.sh
@@ -1,8 +1,1 @@
-export AUTH='6359c8de-0232-4a66-9f74-3c1ff2ced837'
-export BASE_URL="https://shuffle-backend-stbuwivzoq-nw.a.run.app"
-export ORG="a58f92e2-24e3-463c-927e-5977afa8ef31"
-export ENVIRONMENT_NAME="something"
-export DOCKER_API_VERSION=1.40
 
-cd functions/onprem/orborus
-go run orborus.go
